7048684507291d60fc381c4401e350fa24312eea
Stephen Malek <malek.stephen44@gmail.com>
2018-11-12 19:59:46 -0700

    Get counter to increment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,15 @@ class Counter extends Component {
     tags: []
   };
 
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
@@ -18,7 +27,28 @@ class Counter extends Component {
   }
 
   render() {
-    return <div>{this.renderTags() || "There are no tags!"}</div>;
+    return (
+      <div>
+        {this.renderTags() || "There are no tags!"}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 }
 
