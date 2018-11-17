1077d7e78df40221c62a8f36150245b8e6de2c6c
Stephen Malek <malek.stephen44@gmail.com>
2018-11-17 15:19:19 -0700

    Place the state in the app component and pass it down through props

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
